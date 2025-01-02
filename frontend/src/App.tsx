940508686d878fee11715b56ac2435879b09ee5b
Bohredd <dapereira@inf.ufsm.br>
2025-01-02 11:59:13 -0300

    feat: prototype share whatsapp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { lazy } from "react";
+
+const Home = lazy(() => import("./views/Home"));
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<h1>About</h1>} />
       </Routes>
     </Router>
